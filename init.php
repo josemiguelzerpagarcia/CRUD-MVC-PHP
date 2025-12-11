54a6e3bb09de8fb0481ac4550978fc3a8695ca29
Jose Miguel Zerpa Garcia <zerpa.garcia.jose.miguel@iescamas.es>
2025-12-11 11:45:15 +0100

    Subimos los cambios hechos

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/Sao_Paulo');
+
+// conexão com o banco de dados
+
+define('BD_SERVIDOR','fdb1034.awardspace.net');
+define('BD_USUARIO','4706091_livraria');
+define('BD_SENHA','Elreytilin88');
+define('BD_BANCO','4706091_livraria');
+
